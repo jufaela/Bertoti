4afed57787b3ac4378eeb3b020f9333e3dc74317
jufaela <julia.silva71@fatec.sp.gov.br>
2023-03-08 19:55:54 -0300

    estrategia

---
+++ ProjetosDeSistema/estrategia/patterns/MudarPreco.java
@@ -0,0 +1,11 @@
+package ProjetosDeSistema.estrategia.patterns;
+
+public class MudarPreco implements Preco {
+	
+    public void DarPreco() {
+		
+		System.out.println("Voce alterou o preço deste produto");
+		
+	}
+    
+}
