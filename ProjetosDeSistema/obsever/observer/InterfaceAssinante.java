082f64cf81f1de74e391d934f0b324a43a292a27
Júlia Rafaela <79148564+jufaela@users.noreply.github.com>
2023-03-14 12:43:05 -0300

    Delete ProjetosDeSistema/obsever/observer directory

--- ProjetosDeSistema/obsever/observer/InterfaceAssinante.java
+++
@@ -1,4 +0,0 @@
-public interface InterfaceAssinante {
-    public void updateAssinatura(String assinatura);
-    public void updateAtualizacao(String atualizacao);
-}
--- ProjetosDeSistema/obsever/observer/InterfaceJogo.java
+++
@@ -1,6 +0,0 @@
-public interface InterfaceJogo {
-    public void addAssinante(AssinanteObserver assinante);
-    public void removeAssinante(AssinanteObserver assinante);
-    public void notifyAllAssinatura();
-    public void notifyAllAtualizacao();
-}
